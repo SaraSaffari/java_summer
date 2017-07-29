e67add17cb3432ca57910e867406cdb7edfa89fc
sara saff <sarasaffari.1997@gmail.com>
2017-07-29 12:21:46 +0430

    class course is ok

--- online wizardry/src/entities/main.java
+++ online wizardry/src/entities/main.java
@@ -6,17 +6,17 @@ public class main {
 	public static void main(String[] args) {
 		
 		//Scanner reader = new Scanner(System.in); 
-		
 		//System.out.println("Enter a name: ");
 		//String input_name = reader.nextLine();
 		//System.out.println("Enter a number: ");
 		//int input_number = reader.nextInt(); 
 		//reader.close();
-		Person p1 = new Person("gholam", 123, "hogwards", "half_blood");
-		System.out.println(p1.getName());
-		System.out.print(p1.getStudentNumber());
-		System.out.println(p1.getHouseName());
-		System.out.println(p1.getBloodStatus());
+		
+		Course c1 = new Course("magic", "ph", '2',1997);
+		System.out.println(c1.getName());
+		System.out.println(c1.getProfessorName());
+		System.out.println(c1.getMinGrade());
+		System.out.println(c1.getYear());
 		
 	}
 

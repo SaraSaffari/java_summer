13f566407c18b47e6578c998d221e1a4a71ff1cd
sara saff <sarasaffari.1997@gmail.com>
2017-07-29 21:58:10 +0430

    class school is ok

--- online wizardry/src/entities/main.java
+++ online wizardry/src/entities/main.java
@@ -1,5 +1,8 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 //import java.util.Scanner;
 
 public class main {
@@ -12,11 +15,25 @@ public class main {
 		//int input_number = reader.nextInt(); 
 		//reader.close();
 		
-		Course c1 = new Course("magic", "ph", '2',1997);
-		System.out.println(c1.getName());
-		System.out.println(c1.getProfessorName());
-		System.out.println(c1.getMinGrade());
-		System.out.println(c1.getYear());
+		ArrayList <String> shouses = new ArrayList<String>();
+		ArrayList <String> scourses = new ArrayList<String>();
+		Vector <String> sstudents = new Vector<String>();
+		Vector <String> sprofessors = new Vector<String>();
+		shouses.add("kazaban");
+		shouses.add("kazaban");
+		scourses.add("magic");
+		scourses.add("flying");
+		sstudents.add("hp");
+		sstudents.add("ghamar");
+		sprofessors.add("severus");
+		sprofessors.add("dark master");
+		
+		School s1 = new School("hogwarts",shouses ,scourses ,sstudents , sprofessors);
+		System.out.println(s1.getName());
+		System.out.println(s1.getCourseNames());
+		System.out.println(s1.getHouseNames());
+		System.out.println(s1.getProfessorNames());
+		System.out.println(s1.getStudentNames());
 		
 	}
 
